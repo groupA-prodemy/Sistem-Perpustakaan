aeb656b4a566125589e604f9dbb3fa83788af01a
ridwan18001 <mini.project.kelompok1@gmail.com>
2022-09-12 16:17:27 +0700

    miniProject

---
+++ SistemPerpustakaan/SistemPerpustakaan/src/test/java/com/example/SistemPerpustakaan/SistemPerpustakaanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemPerpustakaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemPerpustakaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
